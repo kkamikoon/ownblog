7272b86a1b08b0cc0c3bd8b8de90d14f700dc7bd
kkamikoon <rootsik1221@gmail.com>
2021-07-07 07:10:58 +0000

    fix tags error of flask_sqlalchemy in_() function.

--- app/main/tags.py
+++ app/main/tags.py
@@ -24,22 +24,33 @@ def tags():
             tag_list= selected_tags.split(" ")
             tags    = db.session.query( Tags.name,
                                         Tags.post_idx).filter(  Tags.name.in_(tag_list)).all()
+
+            p_idxs  = [ tag.post_idx for tag in tags if tag.post_idx != None ]
+
+            # Do not change the order of tag list
+            ordered_tags = []
+
+            for v in tag_list:
+                if v not in ordered_tags:
+                    ordered_tags.append(v)
+
         except Exception as e:
             flash(message="You've been wrong action for selecting tags", category="warning")
             return redirect(url_for(".tags"))
 
-        posts   = db.session.query( Posts.idx,
-                                    Posts.title,
-                                    Posts.date).filter( Tags.post_idx.in_([ tag.post_idx for tag in tags ]),
-                                                        Posts.idx==Tags.post_idx,
-                                                        Posts.hidden==False).group_by(Tags.post_idx).all()
+        try:
+            # if the tuple(or list) is None or empty, in_ function occurring error.
+            # https://stackoverflow.com/questions/35375179/sql-alchemy-tuple-func-in-queries-and-none-values
+            # So... i choose the list value about p_idxs to [0] if it is `None` value.
+            posts   = db.session.query( Posts.idx,
+                                        Posts.title,
+                                        Posts.date).filter( Tags.post_idx.in_([0] if p_idxs == [] else p_idxs),
+                                                            Posts.idx==Tags.post_idx,
+                                                            Posts.hidden==False).group_by(Tags.post_idx).all()
+        except Exception as e:
+            flash(message="Unknown error on searching posts.", category="error")
+            return redirect(url_for(".tags"))
 
-        # Do not change the order
-        ordered_tags = []
-
-        for v in tag_list:
-            if v not in ordered_tags:
-                ordered_tags.append(v)
 
         return render_template( f"/front/{get_config('front_theme')}/tags/index.html",
                                 posts=posts,
