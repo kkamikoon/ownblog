7e2760c066d969d67efca9813568b6d970903ec5
kkamikoon <rootsik1221@gmail.com>
2021-08-03 17:39:16 +0000

    Add handler, google service

--- app/admin/configs.py
+++ app/admin/configs.py
@@ -47,6 +47,22 @@ def configs_web():
     set_config("admin_theme",  admin_theme)
     set_config("front_theme",  front_theme)
 
+    # About Post
+    # User can only connect this domain if you `domain_check` set True
+    about_post_idx  = request.form.get("about_post_idx", type=int, default=None)
+    set_config('about_post_idx',  about_post_idx)
+
+    # Post page size
+    post_page_size  = request.form.get("post_page_size", type=int, default=None)
+    set_config('post_page_size',  post_page_size)
+
+    return redirect(url_for("admin.configs"))
+
+
+
+@admin.route("/admin/configs/google", methods=['POST'])
+@admin_only
+def configs_google():
     # Recaptcha (Key & Status)
     recaptcha_site_key  = request.form.get("recaptcha_site_key")
     recaptcha_secret_key= request.form.get("recaptcha_secret_key")
@@ -63,17 +79,12 @@ def configs_web():
     set_config('recaptcha_secret_key',  recaptcha_secret_key)
     set_config('recaptcha_status',      recaptcha_status)
 
-    # About Post
-    # User can only connect this domain if you `domain_check` set True
-    about_post_idx  = request.form.get("about_post_idx", type=int, default=None)
-    set_config('about_post_idx',  about_post_idx)
+    # Google Analytics 
+    analytics_id        = request.form.get("analytics_id")
 
-    # Post page size
-    post_page_size  = request.form.get("post_page_size", type=int, default=None)
-    set_config('post_page_size',  post_page_size)
-
-    return redirect(url_for("admin.configs"))
+    set_config('analytics_id',          analytics_id)
     
+    return redirect(url_for("admin.configs"))
 
 
 @admin.route("/admin/configs/users", methods=['POST'])
@@ -153,6 +164,21 @@ def configs_sns():
 
 
 
+@admin.route("/admin/configs/google/<service_type>", methods=['GET'])
+@admin_only
+def configs_google_status(service_type):
+    status = get_config(f"set_google_{service_type}")
+
+    if status == None:
+        set_config(f"set_google_{service_type}", True)
+        return str(True)
+    
+    set_config(f"set_google_{service_type}", not status)
+
+    return str(not bool(status))
+
+
+
 @admin.route("/admin/configs/sns/<sns_type>", methods=['GET'])
 @admin_only
 def configs_sns_status(sns_type):
@@ -165,5 +191,7 @@ def configs_sns_status(sns_type):
     set_config(f"{sns_type}_open", not status)
 
     return str(not bool(status))
+
+
 
 
